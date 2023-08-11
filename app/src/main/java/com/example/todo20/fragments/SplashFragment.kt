babde98652809bbbc6eca1dab7d5c533ea1fcacd
bishalpb2004 <129422706+bishalpb2004@users.noreply.github.com>
2023-08-11 12:21:26 +0530

    Todo project

---
+++ app/src/main/java/com/example/todo20/fragments/SplashFragment.kt
@@ -0,0 +1,35 @@
+package com.example.todo20.fragments
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.todo20.R
+
+
+class SplashFragment : Fragment() {
+    private lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        Handler(Looper.myLooper()!!).postDelayed({
+             navController.navigate(R.id.action_splashFragment_to_homeFragment)
+        },5000)
+    }
+
+
+}
